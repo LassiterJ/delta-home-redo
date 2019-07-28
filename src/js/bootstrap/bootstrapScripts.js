6942bad20c1207a9c891921e77c7dc8c8ac0ffbf
Jonathan Lassiter <pikapp336@me.com>
2019-07-27 23:16:22 -0400

    played around with bootstrap and worked on nav.

--- src/js/bootstrap/bootstrapScripts.js
+++ src/js/bootstrap/bootstrapScripts.js
@@ -1,10 +1,9 @@
 
 
 $(document).ready(() => {
-    
     $(".more-details-btn").on("click", () => {
         $(".nav-dropdown").toggle();
-        console.log(".nav-dropdown has been triggered");
+       
     })
     
 });
